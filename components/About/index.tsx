b5bbe655c40ce3f1b42f2b235dd31592618d0fbe
Girts Karcevskis <girts521@gmail.com>
2023-10-09 16:15:05 +0200

    Start working on the  hero banner

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -4,18 +4,36 @@ import Image from "next/image";
 export default function About() {
   return (
     <div id="aboutSection" className={styles.aboutContainer}>
-      <Image src="/images/placeholder.webp" alt="hero" width={600} height={400} />
+      <h1>About Me</h1>
+
       <div className={styles.aboutDescription}>
-        <h1>About Me</h1>
+      <Image
+        src="/Photo.png"
+        alt="hero"
+        width={220}
+        height={370}
+      />
         <p>
-          lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Sunt laborum deserunt sit
-          quia autem. Nisi, blanditiis, saepe error repudiandae expedita placeat
-          necessitatibus nemo porro veritatis consequatur eos est, fugiat
-          obcaecati? Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-          Numquam laboriosam esse expedita natus enim atque harum laudantium
-          molestias? Ea consequuntur expedita officiis nostrum, sapiente sequi
-          et aliquam repellat molestiae laborum!
+          I'm originally from Latvia, a charming little country in Eastern
+          Europe. Straight after high school, I left home to explore new
+          cultures, study, and work in different countries - England, Germany,
+          the Netherlands, and China. It's been a journey full of learning and
+          growth.During my travels, I found myself drawn to programming. It
+          started with writing batch scripts to solve problems that didn't have
+          off-the-shelf solutions. The more I researched and tinkered, the more
+          I fell in love with coding.To build on my interest, I took "The Web
+          Developer Bootcamp" on Udemy. This course introduced me to HTML, CSS,
+          JS, NodeJS, Express, and MongoDB. But I didn't stop there. I continued
+          to learn about Linux, ElectronJs, Python, Docker, and even AWS through
+          different courses.I realized that web development was the area I
+          wanted to focus on. So, I took an online bootcamp back in Latvia to
+          learn more about JS, React, and TypeScript. I'm also currently
+          studying Algorithms and Data Structures.In recent years, I've
+          dedicated a significant amount of my free time to learning about
+          different technologies. This investment in my skills led to a
+          rewarding opportunity as a web developer. Now, as I continue to grow
+          in this role, I'm excited to take on new challenges and seize fresh
+          opportunities in the tech industry.
         </p>
       </div>
     </div>
--- components/HeroBanner/index.tsx
+++ components/HeroBanner/index.tsx
@@ -12,35 +12,10 @@ import SkillsBanner from "../SkillsBanner";
 export default function HeroBanner() {
     return (
       <div id="homeSection" className={styles.heroBanner}>
-        <div className={styles.description}>
-          <div className={styles.left}>
             <h1>Full-stack Developer</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Laboriosam provident reprehenderit dolorum minima ad ipsa
-              doloremque dolorem blanditiis dolore quis beatae aliquid
-              reiciendis natus neque, odio, necessitatibus quae magnam soluta.
-            </p>
-          </div>
-          <div className={styles.image}>
-              <Lottie
-                  play
-                  loop
-                  animationData={CircleAnimationData}
-                  style={{ height: '600px', width: '550px' }}
-                  className={styles.lottie}
-              />
-            <Image src="/images/hero.webp" alt="hero" width={416} height={500} />
-          </div>
-        </div>
-        <SkillsBanner />
-          <Lottie
-              play
-              loop
-              animationData={WaveAnimationData}
-              style={{ height: '600px', width: '100%' }}
-              className={styles.lottieWave}
-          />
+            <h2>
+            Magento 2 and React Developer Actively Enhancing the E-commerce Landscape
+            </h2>
       </div>
   );
 }
